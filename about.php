9945f670fb44b80e9177d867c51c000f02e6ddeb
Naveed Ul Aziz <naveedulaziz@gmail.com>
2025-06-14 00:59:46 +0500

    Added the commit from another extions of the project

--- about.php
+++ about.php
@@ -1,9 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-	<style>
-	</style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+<style>
+    .jumbotron2
+    {
+        height: 230px;
+        background-color:black;
+        text-align: center;
+    }
+
+    #app
+    {
+        border-radius: 10px;
+    }
+
+
+
+
+</style>
+
   </head>
+
   <body>
 <?php include 'navbar.php' ?>
 <section>
